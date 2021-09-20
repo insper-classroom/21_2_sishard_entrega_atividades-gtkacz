99f2016a5dd5c142ebc7d16a0474f3f6ef576ff3
Tkacz <gabrielmt2@al.insper.edu.br>
2021-09-20 14:43:20 -0300

    atv 3 ex1'

--- 03-condicionais-funcoes/solucao_ex1.c
+++ 03-condicionais-funcoes/solucao_ex1.c
@@ -1,3 +1,32 @@
-/* Implementar função solucao_ex1 */
+// Dump of assembler code for function ex1:
+//    0x0000000000000000 <+0>:     lea    (%rdi,%rsi,1),%rax
+//    0x0000000000000004 <+4>:     lea    (%rax,%rdx,4),%rcx
+//    0x0000000000000008 <+8>:     imul   %rdi,%rdi
+//    0x000000000000000c <+12>:    lea    (%rdi,%rsi,2),%rax
+//    0x0000000000000010 <+16>:    add    %rax,%rdx
+//    0x0000000000000013 <+19>:    cmp    %rdx,%rcx
+//    0x0000000000000016 <+22>:    setge  %al
+//    0x0000000000000019 <+25>:    movzbl %al,%eax
+//    0x000000000000001c <+28>:    retq
+// End of assembler dump.
 
+//rdi = long a;
+//rsi = long b;
+//rdx = long c;
+//rcx = long d;
+//rax = long r;
+//eax = int f;
 
+int solucao_ex1(long a, long b, long c){
+    long r, d;
+    int f;
+
+    r = a + b;
+    d = r + c * 4;
+    a *= a;
+    r = a + b * 2;
+    c += r;
+    f = d >= c;
+
+    return f;
+}
