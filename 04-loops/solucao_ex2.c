aa2af76d06c7f0141649f4f7749bde2454ab8d22
Tkacz <gabrielmt2@al.insper.edu.br>
2021-09-22 15:25:18 -0300

    atv 4 ex2

--- 04-loops/solucao_ex2.c
+++ 04-loops/solucao_ex2.c
@@ -1,6 +1,30 @@
+// Dump of assembler code for function soma_n:
+//    0x0000000000000000 <+0>:     mov    $0x0,%edx
+//    0x0000000000000005 <+5>:     mov    $0x0,%eax
+//    0x000000000000000a <+10>:    jmp    0x15 <soma_n+21>
+//    0x000000000000000c <+12>:    movslq %edx,%rcx
+//    0x000000000000000f <+15>:    add    %rcx,%rax
+//    0x0000000000000012 <+18>:    add    $0x1,%edx
+//    0x0000000000000015 <+21>:    cmp    %edi,%edx
+//    0x0000000000000017 <+23>:    jl     0xc <soma_n+12>
+//    0x0000000000000019 <+25>:    repz retq
+// End of assembler dump.
 
-/* Implemente aqui sua versão em C da função soma_n
- * presente no arquivo ex2.o
- *
- * Chame sua função de soma_n_solucao */
-    
+//edi = int a;
+//edx = int b;
+//rcx = long c;
+//rax = long r;
+
+int soma_n_solucao(long c, int a){
+    long r;
+    int b = 0;
+    int ri = 0;
+
+    while(b < a){
+        c = (long) b;
+        r += c;
+        b++;
+    }
+
+    return r;
+}
