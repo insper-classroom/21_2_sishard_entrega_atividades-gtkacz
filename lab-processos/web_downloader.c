2a8519ac3ded1bbee9e44b0ebc0cc23e51bcc5ca
Tkacz <gabrielmt2@al.insper.edu.br>
2021-12-06 21:32:05 -0300

    vamo por favor

--- lab-processos/web_downloader.c
+++ lab-processos/web_downloader.c
@@ -23,66 +23,68 @@ void slice_str(const char *str, char *buffer, size_t start, size_t end){
     buffer[j] = 0;
 }
 
+void slice_str(const char * str, char * buffer, size_t start, size_t end){
+    size_t j = 0;
+    for ( size_t i = start; i <= end; ++i ) {
+        buffer[j++] = str[i];
+    }
+    buffer[j] = 0;
+}
+
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream){
+    size_t written = fwrite(ptr, size, nmemb, stream);
+    return written;
+}
+
 int main(int argc, char *argv[]) {
-    pid_t parallel;
+    pid_t parallel_f;
 
-    if (strcmp(argv[1], "-f")){
+    if (strcmp(argv[1], "-f") == 0){
         FILE* input = fopen(argv[2], "r");
-		int len_file = 0;
-        char line[1024];
-        char arr[5][1024] = {""};
+		int len_i = 0;
+        char line[512];
+        char arr[5][512] = {""};
 
-        while(fgets(line, sizeof(line), input)){
-            strcpy(arr[len_file], line);
-            len_file ++;
+        while (fgets(line, sizeof(line), input)){
+            strcpy(arr[len_i], line);
+            len_i++;
         }
- 
 
-        for (int i = 0; i < len_file; i++){
-            char url_c[1024];
-            int verifica = 0;
-			int result;
-            FILE *output;
+        for (int i = 0; i < len_i; i++){
+            char url_c[512];
+            int result;
+			FILE *output;
             CURL *curl;
-            
-            parallel = fork();
 
-            if (parallel){
+            parallel_f = fork();
+			
+            if (parallel_f == 0){
+				int idx_dot = 0;
+
                 int b=0;
                 for (b=0; arr[i][b]!='\0'; b++);
                 if ((arr[i][b-1] = '\n')) {
                     arr[i][b-1] = '\0';
                 }
 
-                for (int k = 0; k < 6; k++){
-                    if (arr[i][k] == 's'){
-                        verifica = 1;
-                    }
-                }
+                slice_str(arr[i], url_c, 7, strlen(arr[i]));
 
-                if (verifica == 1){
-                    slice_str(arr[i], url_c, 8, strlen(arr[i]));
-                } else {
-                    slice_str(arr[i], url_c, 7, strlen(arr[i]));
-                }
-            
-                int dot = 0;
-
-                for (int a= 0; a < strlen(url_c); a++){
-                    if (url_c[a] == '.'){
-                        dot = a;
+                for (int x = 0; x < strlen(url_c); x++){
+                    if (url_c[x] == '.'){
+                        idx_dot = x;
                     }
                     
                 }
          
                 for (int p = 0; p < strlen(url_c); p++){
-                    if ((url_c[p] == '.' || url_c[p] == '/') && (p != dot)){
+                    if ((url_c[p] == '.' || url_c[p] == '/') && (p != idx_dot)){
                         url_c[p] = '_';
                     }
                 }   
                 
                 output = fopen(url_c, "wb");
 
+				curl_global_init(CURL_GLOBAL_DEFAULT);
                 curl = curl_easy_init();
 
                 if(curl){
@@ -109,7 +111,7 @@ int main(int argc, char *argv[]) {
                 }
                   
             } 
-            else {
+            else{
                 int wst;
                 wait(&wst);
             }
@@ -117,40 +119,28 @@ int main(int argc, char *argv[]) {
     }
 	else{
         char url_c[50];
-		int verifica = 0;
-        int result;
         FILE *output;
         CURL *curl;
+        int result;
+		int idx_dot = 0;
 
-        for (int i = 0; i < 6; i++){
-            if (argv[1][i] == 's'){
-                verifica = 1;
-            }
-        }
-
-        if (verifica == 1){
-            slice_str(argv[1], url_c, 8, strlen(argv[1]));
-        } else {
-            slice_str(argv[1], url_c, 7, strlen(argv[1]));
-        }
+        slice_str(argv[1], url_c, 7, strlen(argv[1]));
        
-        int dot = 0;
-
         for (int i = 0; i < strlen(url_c); i++){
             if (url_c[i] == '.'){
-                dot = i;
+                idx_dot = i;
             }
-            
         }
-
+        
         for (int i = 0; i < strlen(url_c); i++){
-            if ((url_c[i] == '.' || url_c[i] == '/') && (i != dot)){
+            if ((url_c[i] == '.' || url_c[i] == '/') && (i != idx_dot)){
                 url_c[i] = '_';
             }
         }
-
+       
         output = fopen(url_c, "wb");
 
+		curl_global_init(CURL_GLOBAL_DEFAULT);
         curl = curl_easy_init();
         curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, output);
@@ -162,7 +152,8 @@ int main(int argc, char *argv[]) {
             printf("URL %s baixada com sucesso.\n", argv[1]);
             fclose(output);
             curl_easy_cleanup(curl);
-        } else{
+        }
+		else{
             printf("Erro: %s\n", curl_easy_strerror(result));
         }  
     }
